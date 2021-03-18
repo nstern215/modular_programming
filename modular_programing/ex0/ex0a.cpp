70136e89129733343fe3472319b836e7f3ccc344
Netanel Stern <netanel@ezsave.co.il>
2021-03-18 22:44:10 +0200

    fix variables names

--- modular_programing/ex0/ex0a.cpp
+++ modular_programing/ex0/ex0a.cpp
@@ -4,14 +4,14 @@
  * Written by: Netanel Stern, id = 206342255, login = netanelst
  *
  * This program reading from the user names of 3 files:
- * inputFilename, encryptionKeyFilename, encryptedFileFilename
+ * input_filename, encryptionkey_filename, encryptedFileFilename
  *
  * the program reading characters from the input file, and using the data
  * from the encryption key file, it encrypts the file and send the encrypted
  * data to the output file
  *
  * input:
- * names of 3 file: encryptionKeyFilename, inputFilename, encryptedFileFilename
+ * names of 3 file: encryptionkey_filename, input_filename, encryptedFileFilename
  *
  * output:
  * encrypted content in the output file
@@ -31,38 +31,38 @@ using std::setw;
 using std::ifstream;
 using std::ofstream;
 
-//----------------functions section
-bool open_files(char inputFilename[], char keyFilename[], char outputFilename[], ifstream& inputIsr, ifstream& keyIst, ofstream& outputOstr);
-void close_files(ifstream& inputIsr, ifstream& keyIst, ofstream& outputOstr);
-void encrypt_file(ifstream& inputFile, ifstream& encryptionKeyFile, ofstream& outputFile);
-char encrypt_char(char toEncrypt, ifstream& encryptionKeyFile);
-
 //----------------constant section----------------
 const int FILENAME_LENGTH = 100;
+
+//----------------functions section
+bool open_files(char input_filename[], char key_filename[], char output_filename[], ifstream& input_isr, ifstream& key_ist, ofstream& output_ostr);
+void close_files(ifstream& input_isr, ifstream& key_ist, ofstream& output_ostr);
+void encrypt_file(ifstream& input_file, ifstream& encryption_key_file, ofstream& output_file);
+char encrypt_char(char to_encrypt, ifstream& encryption_key_file);
 
 int main()
 {
 	//----------------variables section----------------
-	ifstream inputFile;
-	ifstream encryptionKeyFile;
+	ifstream input_file;
+	ifstream encryption_key_file;
 	ofstream encryptedFile;
 
-	char inputFilename[FILENAME_LENGTH];
-	char encryptionKeyFileName[FILENAME_LENGTH];
+	char input_filename[FILENAME_LENGTH];
+	char encryptionkey_filename[FILENAME_LENGTH];
 	char encryptedFileFilename[FILENAME_LENGTH];
 
 	cout << "Enter encryption key filename, input filename, output filename" << endl;
 	
-	cin >> setw(FILENAME_LENGTH) >> encryptionKeyFileName
-		>> setw(FILENAME_LENGTH) >> inputFilename
+	cin >> setw(FILENAME_LENGTH) >> encryptionkey_filename
+		>> setw(FILENAME_LENGTH) >> input_filename
 		>> setw(FILENAME_LENGTH) >> encryptedFileFilename;
 
-	if (!open_files(inputFilename, encryptionKeyFileName, encryptedFileFilename, inputFile, encryptionKeyFile, encryptedFile))
+	if (!open_files(input_filename, encryptionkey_filename, encryptedFileFilename, input_file, encryption_key_file, encryptedFile))
 		return EXIT_FAILURE;
 
-	encrypt_file(inputFile, encryptionKeyFile, encryptedFile);
+	encrypt_file(input_file, encryption_key_file, encryptedFile);
 
-	close_files(inputFile, encryptionKeyFile, encryptedFile);
+	close_files(input_file, encryption_key_file, encryptedFile);
 	
 	return EXIT_SUCCESS;
 }
@@ -73,37 +73,37 @@ int main()
  * this function use to open files
  * 
  * parameters:
- * inputFilename: path to input file
- * keyFilename: path encryption key file
- * outputFilename: path to output file
+ * input_filename: path to input file
+ * key_filename: path encryption key file
+ * output_filename: path to output file
  * inputIstr: reference for input file
- * keyIstr: reference for encryption key file
- * outputOstr: reference for output file
+ * key_istr: reference for encryption key file
+ * output_ostr: reference for output file
  *
  * output:
  * If all files has open success - true
  * Otherwise - false
  */
-bool open_files(char inputFilename[], char keyFilename[], char outputFilename[], ifstream& inputIsr, ifstream& keyIst, ofstream& outputOstr)
+bool open_files(char input_filename[], char key_filename[], char output_filename[], ifstream& input_isr, ifstream& key_ist, ofstream& output_ostr)
 {
-	inputIsr.open(inputFilename);
-	if(!inputIsr.is_open())
+	input_isr.open(input_filename);
+	if(!input_isr.is_open())
 	{
-		cerr << "Cannot open input file " << inputFilename << endl;
+		cerr << "Cannot open input file " << input_filename << endl;
 		return false;
 	}
 
-	keyIst.open(keyFilename);
-	if(!keyIst.is_open())
+	key_ist.open(key_filename);
+	if(!key_ist.is_open())
 	{
-		cerr << "Cannot open encryption key file " << keyFilename << endl;
+		cerr << "Cannot open encryption key file " << key_filename << endl;
 		return false;
 	}
 
-	outputOstr.open(outputFilename);
-	if (!outputOstr.is_open())
+	output_ostr.open(output_filename);
+	if (!output_ostr.is_open())
 	{
-		cerr << "Cannot open output file " << outputFilename << endl;
+		cerr << "Cannot open output file " << output_filename << endl;
 		return false;
 	}
 
@@ -114,14 +114,14 @@ bool open_files(char inputFilename[], char keyFilename[], char outputFilename[],
  * This function uses to close the open files
  * parameters:
  * inputIstr: reference for input file
- * keyIstr: reference for encryption key file
- * outputOstr: reference for output file
+ * key_istr: reference for encryption key file
+ * output_ostr: reference for output file
  */
-void close_files(ifstream& inputIsr, ifstream& keyIst, ofstream& outputOstr)
+void close_files(ifstream& input_isr, ifstream& key_ist, ofstream& output_ostr)
 {
-	inputIsr.close();
-	keyIst.close();
-	outputOstr.close();
+	input_isr.close();
+	key_ist.close();
+	output_ostr.close();
 }
 
 /*
@@ -129,31 +129,30 @@ void close_files(ifstream& inputIsr, ifstream& keyIst, ofstream& outputOstr)
  *
  * parameters:
  * inputIstr: reference for input file
- * keyIstr: reference for encryption key file
- * outputOstr: reference for output file
+ * key_istr: reference for encryption key file
+ * output_ostr: reference for output file
  *
  * the function reading a single char from the input file. if this character is not an alphanumeric char,
  * its send to the output file and the function continue to the next char in the file.
  *
  * if it is, the function sending the char for encryption
  */
-void encrypt_file(ifstream& inputFile, ifstream& encryptionKeyFile, ofstream& outputFile)
+void encrypt_file(ifstream& input_file, ifstream& encryption_key_file, ofstream& output_file)
 {
+	char output_char;
+	char input_char = input_file.get();
 	
-	char outputChar;
-	char inputChar = inputFile.get();
-	
-	while(!inputFile.eof())
+	while(!input_file.eof())
 	{
 		//if the char is not relevant for the encryption, sent for encryption, otherwise, use the original char
-		if (isalpha(inputChar))
-			outputChar = encrypt_char(inputChar, encryptionKeyFile);
+		if (isalpha(input_char))
+			output_char = encrypt_char(input_char, encryption_key_file);
 		else
-			outputChar = inputChar;
+			output_char = input_char;
 
-		outputFile << outputChar;
+		output_file << output_char;
 		
-		inputChar = inputFile.get();
+		input_char = input_file.get();
 	}
 }
 
@@ -165,29 +164,29 @@ void encrypt_file(ifstream& inputFile, ifstream& encryptionKeyFile, ofstream& ou
  * if, it check if the char from the input file and the char from the key are equal, and if so, the char from
  * the input replacing by the *next* char from the key
  */
-char encrypt_char(char toEncrypt, ifstream& encryptionKeyFile)
+char encrypt_char(char to_encrypt, ifstream& encryption_key_file)
 {
-	char encryptKeyChar = encryptionKeyFile.get();
-	char outputChar = toEncrypt; //outputChar use to store the output char (original from input or encrypted)
+	char encrypt_key_char = encryption_key_file.get();
+	char output_char = to_encrypt; //output_char use to store the output char (original from input or encrypted)
 
 	//reading chars from the encryption key and look for a match with the char from the input file
-	while (!encryptionKeyFile.eof())
+	while (!encryption_key_file.eof())
 	{
-		if (isalpha(encryptionKeyFile.peek()))
+		if (isalpha(encryption_key_file.peek()))
 		{
-			if (toEncrypt == encryptKeyChar)
+			if (to_encrypt == encrypt_key_char)
 			{
-				outputChar = encryptionKeyFile.peek();
+				output_char = encryption_key_file.peek();
 				break;
 			}
 		}
 
-		encryptKeyChar = encryptionKeyFile.get();
+		encrypt_key_char = encryption_key_file.get();
 	}
 
 	//set back the pointer to the beginning of the encryption key file
-	encryptionKeyFile.clear();
-	encryptionKeyFile.seekg(-encryptionKeyFile.tellg(), std::ios::cur);
+	encryption_key_file.clear();
+	encryption_key_file.seekg(-encryption_key_file.tellg(), std::ios::cur);
 
-	return outputChar;
+	return output_char;
 }
