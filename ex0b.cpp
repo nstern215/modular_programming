37f2eb6da21a4584f053ba2208f0d04cebaeb045
Netanel Stern <netanel@ezsave.co.il>
2021-03-18 16:34:11 +0200

    remove CLion project data

---
+++ ex0b.cpp
@@ -0,0 +1,30 @@
+//------------------include section------------------
+#include <iostream>
+#include <iomanip>
+#include <iostream>
+
+//------------------using section------------------
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::cin;
+using std::setw;
+using std::ifstream;
+using std::ofstream;
+
+//------------------functions section------------------
+bool open_files(char input_filename[], char encrypted_filename[], ifstream& input_file, ifstream& encrypted_file);
+
+//------------------constant section------------------
+const int FILENAME_LENGtH = 100;
+
+int main()
+{
+    return EXIT_SUCCESS;
+}
+
+//------------------functions implementation------------------
+bool open_files(char input_filename[], char encrypted_filename[], ifstream& input_file, ifstream& encrypted_file)
+{
+
+}
